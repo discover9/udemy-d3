d675da4327ccd4bccf0cbdd580ba10d60ff379eb
discover9 <adrious@gmail.com>
2021-12-07 02:34:13 -0500

    Section 2

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,27 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json("data/buildings.json").then(data => {
+  console.log(data)
+  data.forEach(element => {
+      element.height = Number(element.height)
+  });
+
+const svg = d3.select("#chart-area").append("svg")
+.attr("width", 400)
+.attr("height", 400)
+
+const rects = svg.selectAll("rect")
+  .data(data)
+
+rects.enter().append("rect")
+  .attr("x", (element, index) => (index*75)+10)
+  .attr("y", 5)
+  .attr("width", 50)
+  .attr("height", (element) => element.height)
+  .attr("fill", "grey")
+
+}).catch(error => {
+console.log(error)
+})
